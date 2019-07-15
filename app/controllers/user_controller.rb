ee469093b5ada59fc945a1811c73417c599f3328
valentine-mario <valentine13400@gmail.com>
2019-07-15 11:55:14 +0100

    added function to remove admin

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
 class UserController < ApplicationController
     before_action :authorize_request, except: [:createUser, :getAllUsers, :getUserById]
-    before_action :findUser, only:[:setAdmin]
+    before_action :findUser, only:[:setAdmin, :unsetAdmin]
     def createUser
         @new_user=User.new(user_params)
         if @new_user.save
@@ -12,12 +12,26 @@ class UserController < ApplicationController
     end
 
     def setAdmin
-        if @user.isAdmin == true
+        if @current_user.isAdmin == false
+            render :json=>{code:"01", message:"unauthorised to make admin"}, status: :unauthorised
+        elsif @user.isAdmin == true
          render :json=>{code:"01", message:@user.name+ " is already an admin"}, status: :ok
         elsif @user.update(makeAdmin)
             render :json=>{code:"00", message:@user.name+ " is now an admin"}, status: :ok
         else
             render :json=>{code:"01", message:"error making "+ @user.name+ " an admin"}, status: :unprocessable_entity
+        end 
+    end
+
+    def unsetAdmin
+        if @current_user.isAdmin == false
+            render :json=>{code:"01", message:"unauthorised to remove admin"}, status: :unauthorised
+        elsif @user.isAdmin == false
+         render :json=>{code:"01", message:@user.name+ " is not an admin"}, status: :ok
+        elsif @user.update(removeAdmin)
+            render :json=>{code:"00", message:@user.name+ " is no longer an admin"}, status: :ok
+        else
+            render :json=>{code:"01", message:"error removing "+ @user.name+ " an admin"}, status: :unprocessable_entity
         end 
     end
 
@@ -36,6 +50,11 @@ class UserController < ApplicationController
 
     def makeAdmin
         defaults = { isAdmin: true }
+        params.permit(:isAdmin).reverse_merge(defaults)
+    end
+
+    def removeAdmin
+        defaults = { isAdmin: false }
         params.permit(:isAdmin).reverse_merge(defaults)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     get '/makeadmin/:id', :to=>"user#setAdmin"
     post '/login', :to=>"auth#login"
     post '/adminlogin', :to=>"auth#adminLogin"
+    get '/removeadmin/:id', :to=>"user#unsetAdmin"
   end
 end
