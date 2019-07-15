c37bfe72f0b6dfe840afd76b5a3b675c561e6b2a
valentine-mario <valentine13400@gmail.com>
2019-07-15 11:04:35 +0100

    added function to make user admin

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
 class UserController < ApplicationController
     before_action :authorize_request, except: [:createUser, :getAllUsers, :getUserById]
-
+    before_action :findUser, only:[:setAdmin]
     def createUser
         @new_user=User.new(user_params)
         if @new_user.save
@@ -11,12 +11,31 @@ class UserController < ApplicationController
         end
     end
 
+    def setAdmin
+        if @user.isAdmin == true
+         render :json=>{code:"01", message:@user.name+ " is already an admin"}
+        elsif @user.update(makeAdmin)
+            render :json=>{code:"00", message:@user.name+ " is now an admin"}
+        else
+            render :json=>{code:"01", message:"error making "+ @user.name+ " an admin"}
+        end 
+    end
+
 
     private
+
+    def findUser
+        @user = User.find(params[:id])
+      end
     def user_params
         
         params.permit(
            :name, :email, :password, :password_confirmation, :isAdmin
           )
+    end
+
+    def makeAdmin
+        defaults = { isAdmin: true }
+        params.permit(:isAdmin).reverse_merge(defaults)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   scope 'user' do
     post '/add', :to=>"user#createUser"
+    get '/makeadmin/:id', :to=>"user#setAdmin"
   end
 end
